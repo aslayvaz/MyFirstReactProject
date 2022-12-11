704323de41e601fb31d06faf9436117d5cbce9aa
aslayvaz <aslihan_ayvaz@hotmail.com>
2022-12-11 17:33:50 +0300

    addedNavbarComponent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="container">
-      <h2>App Component</h2>
+      <Navbar/>
     </div>
   );
 }
