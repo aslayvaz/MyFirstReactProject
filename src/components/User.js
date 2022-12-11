b19cc10ad516c8fc5dffc5a16d5a5479b2c83230
aslayvaz <aslihan_ayvaz@hotmail.com>
2022-12-11 22:24:03 +0300

    fontAwesome

--- src/components/User.js
+++ src/components/User.js
@@ -12,12 +12,17 @@ class User extends Component {
     //Destructuring 
     const {name, department, salary} = this.props;
     return (
-      <div>
-        <ul>
-          <li>İsim: {name}</li>
-          <li>Departman: {department}</li>
-          <li>Maaş: {salary}</li>
-        </ul>       
+      <div className='col-md-8 mb-4'>
+        <div className='card'>
+          <div className='card-header d-flex justify-content-between'>
+            <h4 className='d-inline'>{name}</h4>
+            <i className="fa-regular fa-trash-can"></i>
+          </div>
+          <div className="card-body">
+            <p className="card-text">Maaş: {salary}</p>
+            <p className="card-text">Departman: {department}</p>
+          </div>
+        </div>        
       </div>
     )
   }
