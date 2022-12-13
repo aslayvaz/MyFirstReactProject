370dc98de8d0673c07112a3e5c7a0df8596d854a
aslayvaz <aslihan_ayvaz@hotmail.com>
2022-12-13 23:11:03 +0300

    communicationBetweenComponents

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import User from './components/User';
+import Users from './components/Users';
 
 function App() {
+  state = {
+    users: [
+      {
+        id: 1,
+        name: "Aslihan Ayvaz",
+        salary: "5000",
+        department: "Bilişim"
+      },
+      {
+        id: 2,
+        name: "Tuncer Ayvaz",
+        department: "Bilişim"
+      },
+      {
+        id: 3,
+        name: "Jhon Doe",
+        department: "Üretim"
+      }
+    ]
+  }
+
   return (
     <div className="container">
-      <Navbar/>
-      <hr/>
-      <User 
-      name = "Aslihan Ayvaz"
-      salary = "5000"
-      department = "Bilişim"
-      />
-      <User 
-      name = "Tuncer Ayvaz"
-      department = "Bilişim"
-      />
-      
+      <Navbar />
+      <hr />
+      <Users users={this.state.users} />
     </div>
   );
 }
